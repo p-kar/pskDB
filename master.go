bd5da48c24bcdc1af6a417138f8b107585922e0b
kartik894 <kartik894@gmail.com>
2018-02-03 18:55:00 -0600

    added KillServer method

--- master.go
+++ master.go
@@ -1,84 +1,111 @@
 package main
 
 import (
-    "bufio"
-    "os"
-    "strings"
-    "os/exec"
-    "strconv"
-    "./logger"
+	"./logger"
+	"bufio"
+	"net/rpc"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
 )
 
+// get RPC client object given an IP address
+func getRPCConnection(address string) *rpc.Client {
+	client, err := rpc.Dial("tcp", address)
+	if err != nil {
+		// log.Warning.Printf("Unable to dial server at address: %s.\n",
+		//     address)
+		return nil
+	}
+	return client
+}
+
 func main() {
-    scanner := bufio.NewScanner(os.Stdin)
-    log := logger.NewLogger("[  MASTER  ] ", os.Stdout, os.Stdout, 
-        os.Stdout, os.Stderr, os.Stderr)
-    // map from node(server) id to its port number
-    serverNodeMap := make(map[string]int)
-    // Starting value of the port numbers used by servers
-    serverNextPort := 9000
-    // map from node(client) id to its port number
-    // clientNodeMap := make(map[string]int)
-    // Starting value of the port numbers used by clients
-    // clientNextPort := 10000
+	scanner := bufio.NewScanner(os.Stdin)
+	log := logger.NewLogger("[  MASTER  ] ", os.Stdout, os.Stdout,
+		os.Stdout, os.Stderr, os.Stderr)
+	// map from node(server) id to its port number
+	serverNodeMap := make(map[string]int)
+	// Starting value of the port numbers used by servers
+	serverNextPort := 9000
+	// map from node(client) id to its port number
+	// clientNodeMap := make(map[string]int)
+	// Starting value of the port numbers used by clients
+	// clientNextPort := 10000
 
-    for scanner.Scan(){
-        // read and process the command from stdin
-        command := scanner.Text()
-        commandSplit := strings.Split(command, " ")
+	for scanner.Scan() {
+		// read and process the command from stdin
+		command := scanner.Text()
+		commandSplit := strings.Split(command, " ")
 
-        if (commandSplit[0] == "done") {
-            log.Info.Println("Goodbye.")
-            break
-        }
+		if commandSplit[0] == "done" {
+			log.Info.Println("Goodbye.")
+			break
+		}
 
-        switch commandSplit[0] {
-            case "joinServer":
-                log.Info.Println("Executing...", commandSplit)
-                
-                // Create startup info i.e., arguments
-                // example arguments ["1" "9001" "9004" "9003"]
-                nodeId := commandSplit[1]
-                args := []string{}
-                args = append(args, nodeId, strconv.Itoa(serverNextPort))
-                for _, port := range serverNodeMap {
-                    args = append(args, strconv.Itoa(port))
-                }
-                serverNodeMap[nodeId] = serverNextPort
-                serverNextPort++
+		switch commandSplit[0] {
+		case "joinServer":
+			log.Info.Println("Executing...", commandSplit)
 
-                // spawn the server
-                joinCmd := exec.Command("./serverNode", args...)
-                joinCmd.Stdout = os.Stdout
-                joinCmd.Stderr = os.Stderr
-                err := joinCmd.Start()
-                if err != nil {
-                    log.Panic.Panicln(err)
-                }
-                log.Info.Println("Started server with process id", joinCmd.Process.Pid)
-                go joinCmd.Wait()
-            case "killServer":
-                log.Info.Println("TODO ", commandSplit)
-            case "joinClient":
-                log.Info.Println("TODO ", commandSplit)
-            case "breakConnection":
-                log.Info.Println("TODO ", commandSplit)
-            case "createConnection":
-                log.Info.Println("TODO ", commandSplit)
-            case "stabilize":
-                log.Info.Println("TODO ", commandSplit)
-            case "printStore":
-                log.Info.Println("TODO ", commandSplit)
-            case "put":
-                log.Info.Println("TODO ", commandSplit)
-            case "get":
-                log.Info.Println("TODO ", commandSplit)
-            default:
-                log.Warning.Println("Command", commandSplit, "not recognized")
-                break
-        }
-    }
-    if err := scanner.Err(); err != nil {
-        log.Panic.Panicln("Standard input error:", err)
-    }
+			// Create startup info i.e., arguments
+			// example arguments ["1" "9001" "9004" "9003"]
+			nodeId := commandSplit[1]
+			args := []string{}
+			args = append(args, nodeId, strconv.Itoa(serverNextPort))
+			for _, port := range serverNodeMap {
+				args = append(args, strconv.Itoa(port))
+			}
+			serverNodeMap[nodeId] = serverNextPort
+			serverNextPort++
+
+			// spawn the server
+			joinCmd := exec.Command("./serverNode", args...)
+			joinCmd.Stdout = os.Stdout
+			joinCmd.Stderr = os.Stderr
+			err := joinCmd.Start()
+			if err != nil {
+				log.Panic.Panicln(err)
+			}
+			log.Info.Println("Started server with process id", joinCmd.Process.Pid)
+			go joinCmd.Wait()
+		case "killServer":
+			log.Info.Println("Executing...", commandSplit)
+			// get Node ID
+			nodeId := commandSplit[1]
+			// get server port number for RPC call
+			if _, ok := serverNodeMap[nodeId]; ok == false {
+				log.Warning.Printf("Server ID: %s is not present in the cluster\n", nodeId)
+				continue
+			}
+			serverPort := serverNodeMap[nodeId]
+			client := getRPCConnection("localhost:" + strconv.Itoa(serverPort))
+			req := true
+			var reply bool
+			client.Call("ServerListener.KillServer", &req, &reply)
+			// log.Info.Println("killServer finished")
+			delete(serverNodeMap, nodeId)
+			client.Close()
+		case "joinClient":
+			log.Info.Println("TODO ", commandSplit)
+		case "breakConnection":
+			log.Info.Println("TODO ", commandSplit)
+		case "createConnection":
+			log.Info.Println("TODO ", commandSplit)
+		case "stabilize":
+			log.Info.Println("TODO ", commandSplit)
+		case "printStore":
+			log.Info.Println("TODO ", commandSplit)
+		case "put":
+			log.Info.Println("TODO ", commandSplit)
+		case "get":
+			log.Info.Println("TODO ", commandSplit)
+		default:
+			log.Warning.Println("Command", commandSplit, "not recognized")
+			break
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Panic.Panicln("Standard input error:", err)
+	}
 }
--- server/structs.go
+++ server/structs.go
@@ -8,66 +8,71 @@ import (
 type Nothing bool
 
 type ServerInfo struct {
-    // unique id of the server
-    Id string
-    // ip address of the server
-    IP_address string
-    // rpc address of the server (IP_address + ":" + Port_num)
-    Address string
-    // port number of the server
-    Port_num string
-    // latest heartbeat sequence number that has been received from this server
-    Heartbeat_seqnum int
-    // timestamp according to the local clock when the last sequence number
-    // from this server was received
-    Timestamp time.Time
-    // boolean flag that is true when the server is alive and false if the 
-    // server is either in suspicion state or is dead
-    Alive bool
-    // boolean flag that is true when the server is in suspicion state
-    Suspicion bool
+	// unique id of the server
+	Id string
+	// ip address of the server
+	IP_address string
+	// rpc address of the server (IP_address + ":" + Port_num)
+	Address string
+	// port number of the server
+	Port_num string
+	// latest heartbeat sequence number that has been received from this server
+	Heartbeat_seqnum int
+	// timestamp according to the local clock when the last sequence number
+	// from this server was received
+	Timestamp time.Time
+	// boolean flag that is true when the server is alive and false if the
+	// server is either in suspicion state or is dead
+	Alive bool
+	// boolean flag that is true when the server is in suspicion state
+	Suspicion bool
 }
 
 func NewServerInfoHeap(serv_info ServerInfo) *ServerInfo {
-    new_server_info := new(ServerInfo)
+	new_server_info := new(ServerInfo)
 
-    new_server_info.Id = serv_info.Id
-    new_server_info.IP_address = serv_info.IP_address
-    new_server_info.Port_num = serv_info.Port_num
-    new_server_info.Address = serv_info.Address
-    new_server_info.Heartbeat_seqnum = serv_info.Heartbeat_seqnum
-    new_server_info.Timestamp = time.Now()
-    new_server_info.Alive = serv_info.Alive
-    new_server_info.Suspicion = serv_info.Suspicion
+	new_server_info.Id = serv_info.Id
+	new_server_info.IP_address = serv_info.IP_address
+	new_server_info.Port_num = serv_info.Port_num
+	new_server_info.Address = serv_info.Address
+	new_server_info.Heartbeat_seqnum = serv_info.Heartbeat_seqnum
+	new_server_info.Timestamp = time.Now()
+	new_server_info.Alive = serv_info.Alive
+	new_server_info.Suspicion = serv_info.Suspicion
 
-    return new_server_info
+	return new_server_info
 }
 
 type JoinClusterAsServerRequest struct {
-    // unique identifier of the server that wants to join
-    Id string
-    // ip address of the server that wants to join
-    IP_address string
-    // port number for the server that wants to join
-    Port_num string
+	// unique identifier of the server that wants to join
+	Id string
+	// ip address of the server that wants to join
+	IP_address string
+	// port number for the server that wants to join
+	Port_num string
 }
 
 // the reply returns a list of servers that are connected in the cluster
 type JoinClusterAsServerReply struct {
-    // list of server already in the cluster
-    ServerInfoList []ServerInfo
+	// list of server already in the cluster
+	ServerInfoList []ServerInfo
 }
 
 type NewServerNotificationRequest struct {
 	// unique identifier of the server that sends the notification
-    Id string
+	Id string
 	// new server info
 	NewServerInfo ServerInfo
 }
 
 type HeartbeatNotificationRequest struct {
-    // unique identifier of the server that sends the notification
-    Id string
-    // list of server already in the cluster
-    ServerInfoList []ServerInfo
+	// unique identifier of the server that sends the notification
+	Id string
+	// list of server already in the cluster
+	ServerInfoList []ServerInfo
+}
+
+type KillServerNotificationRequest struct {
+	// unique id of the server
+	Id string
 }
