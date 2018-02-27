6bc870fead45dfddc3a561fd06e4a7daab5255c0
Pratyush Kar <pratyush.kar@gmail.com>
2018-02-26 19:19:07 -0600

    clients now support multiple server connections

--- client/structs.go
+++ client/structs.go
@@ -1,19 +1,38 @@
 package main
 
 import (
+    "net/rpc"
 )
+
+type ServerInfo struct {
+    // unique id of the server
+    Id string
+    // server address
+    Address string
+    // RPC client object
+    rpcClient *rpc.Client
+}
+
+// declares a NewServerInfo object in heap and returns pointer
+func NewServerInfoHeap(id string, addr string) *ServerInfo {
+    new_server_info := new(ServerInfo)
+
+    new_server_info.Id = id
+    new_server_info.Address = addr
+    new_server_info.rpcClient = nil
+
+    return new_server_info
+}
 
 type ClientInfo struct {
     // unique id of the client
     Id string
     // ip address of the client
     IP_address string
-    // rpc address of the client (IP_address + ":" + Port_num)
-    Address string
     // port number of the client
     Port_num string
-    // unique id of the server
-    Server_Id string
-    // server address
-    Server_Address string
+    // rpc address of the client (IP_address + ":" + Port_num)
+    Address string
+    // map of servers this client is connected to (some might be dead)
+    serv_map map[string]*ServerInfo
 }
